180063af5386d90db97fe372331b07a92cdc031e
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-08-06 21:56:40 -1000

    Added flipping.

--- src/main.c
+++ src/main.c
@@ -24,10 +24,12 @@ __declspec(dllexport)
 #define TEXT_HEIGHT 16
 
 struct {
-    int x;
-    int y;
-    int dx;
-    int dy;
+    int x, y;
+    int dx, dy;
+
+    int scaled;
+    int inverted;
+    int flip_x, flip_y;
 } props;
 
 void initialize_properties() {
@@ -35,102 +37,88 @@ void initialize_properties() {
     props.y = (240 - TEXT_HEIGHT) / 2;
     props.dx = 1;
     props.dy = 2;
+
+    props.inverted = 0;
+    props.scaled = 1;
+    props.flip_x = 0;
+    props.flip_y = 0;
 }
 
 struct {
-    PDMenuItem *menu_item;
-    PDMenuItem *menu_item_check;
-    PDMenuItem *menu_item_options;
+    PDMenuItem *menu_item_invert;
+    PDMenuItem *menu_item_scale;
+    PDMenuItem *menu_item_flip;
 } menu_items;
 
-int scale = 1;
-int scaled = 1;
-int inverted = 0;
-
-void menu_item_check_callback(void *userdata) {
+void menu_item_invert_callback(void *userdata) {
     PlaydateAPI *pd = (PlaydateAPI*) userdata;
-    inverted = inverted ? 0 : 1;
-    pd->display->setInverted(inverted);
+    props.inverted = props.inverted ? 0 : 1;
+    pd->display->setInverted(props.inverted);
     eventHandler(pd, kEventResume, 0);
 }
 
-void menu_item_callback(void *userdata) {
+void menu_item_scale_callback(void *userdata) {
     PlaydateAPI *pd = (PlaydateAPI*) userdata;
-//    scale = (2 * scale) % 8;
-//    if (scale == 0)
-//        scale = 1;
-//
-//    props.x = (400 - TEXT_WIDTH * scale) / 2 / scale;
-//    props.y = (240 - TEXT_HEIGHT * scale) / 2 / scale;
-//    props.dx = 1;
-//    props.dy = 2;
-//
-//    pd->display->setScale(scale);
-    eventHandler(pd, kEventTerminate, 0);
-}
 
-void menu_item_options_callback(void *userdata) {
-    PlaydateAPI *pd = (PlaydateAPI*) userdata;
-    int scaled_idx = pd->system->getMenuItemValue(menu_items.menu_item_options);
-    scaled = 1 << scaled_idx;
+    // Returns 0 -> 1, 1 -> 2, 2-> 4.
+    int scale_idx = pd->system->getMenuItemValue(menu_items.menu_item_scale);
+    props.scaled = 1 << scale_idx;
 
-    props.x = (400 - TEXT_WIDTH * scaled) / 2 / scaled;
-    props.y = (240 - TEXT_HEIGHT * scaled) / 2 / scaled;
+    props.x = (400 - TEXT_WIDTH * props.scaled) / 2 / props.scaled;
+    props.y = (240 - TEXT_HEIGHT * props.scaled) / 2 / props.scaled;
     props.dx = 1;
     props.dy = 2;
 
-    pd->display->setScale(scaled);
+    pd->display->setScale(props.scaled);
+    eventHandler(pd, kEventResume, 0);
+}
+
+void menu_item_flip_callback(void *userdata) {
+    PlaydateAPI *pd = (PlaydateAPI*) userdata;
+
+    // Returns 0 -> no flip, 1 -> flip x, 2 -> flip y, 3 -> flip both.
+    int flip_idx = pd->system->getMenuItemValue(menu_items.menu_item_flip);
+    props.flip_x = (flip_idx % 2) == 1;
+    props.flip_y = flip_idx >= 2;
+
+    pd->display->setFlipped(props.flip_x, props.flip_y);
     eventHandler(pd, kEventResume, 0);
 }
 
 void initialize_menu(PlaydateAPI *pd) {
-    menu_items.menu_item_check = pd->system->addCheckmarkMenuItem("Invert", inverted, menu_item_check_callback, pd);
+    menu_items.menu_item_invert = pd->system->addCheckmarkMenuItem("Invert", props.inverted,
+                                                                   menu_item_invert_callback, pd);
+
     const char *scales[] = {"1", "2", "4"};
-    menu_items.menu_item_options = pd->system->addOptionsMenuItem("Scale", scales, 3, menu_item_options_callback, pd);
-    menu_items.menu_item = pd->system->addMenuItem("Quit", menu_item_callback, pd);
+    menu_items.menu_item_scale = pd->system->addOptionsMenuItem("Scale", scales, 3, menu_item_scale_callback, pd);
+
+    const char *flips[] = {"-", "X", "Y", "XY"};
+    menu_items.menu_item_flip = pd->system->addOptionsMenuItem("Flip", flips, 4, menu_item_flip_callback, pd);
 }
 
 
-int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg)
-{
-    PDButtons current, pushed, released;
-
-//	(void)arg; // arg is currently only used for event = kEventKeyPressed
-    pd->system->logToConsole("arg=%d", arg);
-
-	if ( event == kEventInit )
-	{
-        pd->system->logToConsole("kEventInit\n");
+int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg) {
+	if (event == kEventInit) {
+        pd->system->logToConsole("kEventInit event\n");
 
 		const char* err;
 		font = pd->graphics->loadFont(font_path, &err);
 		
-		if ( font == NULL )
+		if (font == NULL)
 			pd->system->error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__, font_path, err);
 
         initialize_properties();
         initialize_menu(pd);
 
-        // Note: If you set an update callback in the kEventInit handler, the system assumes the game is pure C and
+        // NOTE: If you set an update callback in the kEventInit handler, the system assumes the game is pure C and
         // doesn't run any Lua code in the game
 		pd->system->setUpdateCallback(update, pd);
 	}
 
-//    if (event == kEventTerminate) {
-//        pd->system->logToConsole("kEventTerminate\n");
-//        return 0;
-//    }
-
     if (event == kEventKeyReleased && props.dx < 0) {
-        pd->system->logToConsole("kEventKeyReleased\n");
+        pd->system->logToConsole("kEventKeyReleased event\n");
         props.dx = -props.dx;
     }
-
-//    pd->system->logToConsole("Checking button state...\n");
-//    pd->system->getButtonState(&current, &pushed, &released);
-//    if (current || pushed || released) {
-//        pd->system->logToConsole("Current=%d, Pushed=%d, Released=%d\n", current, pushed, released);
-//    }
 
 	return 0;
 }
@@ -144,17 +132,16 @@ static int update(void* userdata)
 	pd->graphics->setFont(font);
 	pd->graphics->drawText("Hello World!", strlen("Hello World!"), kASCIIEncoding, props.x, props.y);
 
+    // Change the x and y coordinates, flipping the speed if necessary.
 	props.x += props.dx;
 	props.y += props.dy;
-	
-	if (props.x < 0 || props.x * scaled > (LCD_COLUMNS - TEXT_WIDTH * scaled))
+	if (props.x < 0 || props.x * props.scaled > (LCD_COLUMNS - TEXT_WIDTH * props.scaled))
 		props.dx = -props.dx;
-	
-	if (props.y < 0 || props.y * scaled > (LCD_ROWS - TEXT_HEIGHT * scaled))
+	if (props.y < 0 || props.y * props.scaled > (LCD_ROWS - TEXT_HEIGHT * props.scaled))
 		props.dy = -props.dy;
-        
-	pd->system->drawFPS(0,0);
+
+    // Don't display the frames per second.
+    // pd->system->drawFPS(0,0);
 
 	return 1;
 }
-
