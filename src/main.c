2882ea1ce903e9b0ff61458430fea53961a2bef1
Sebastian Raaphorst <sraaphorst@gmail.com>
2023-08-06 23:29:02 -1000

    Crank handling.

--- src/main.c
+++ src/main.c
@@ -1,13 +1,11 @@
-//
-//  main.c
-//  Extension
-//
-//  Created by Dave Hayden on 7/30/14.
-//  Copyright (c) 2014 Panic, Inc. All rights reserved.
-//
+/**
+ * main.c
+ *
+ * By Sebastian Raaphorst, 2023.
+ * Adapted from Playdate Hello World C example.
+ */
 
 #include <stdlib.h>
-
 #include "pd_api.h"
 
 static int update(void* userdata);
@@ -23,9 +21,20 @@ __declspec(dllexport)
 #define TEXT_WIDTH 86
 #define TEXT_HEIGHT 16
 
+// Speed increase for X and Y and maximums.
+#define X_MULTIPLIER_DELTA 2
+#define X_MULTIPLIER_MAX 6
+#define Y_MULTIPLIER_DELTA 1
+#define Y_MULTIPLIER_MAX 3
+
+// The initial movement in each direction.
+#define X_DELTA 2
+#define Y_DELTA 1
+
 struct {
     int x, y;
     int dx, dy;
+    int x_multiplier, y_multiplier;
 
     int scaled;
     int inverted;
@@ -35,8 +44,10 @@ struct {
 void initialize_properties() {
     props.x = (400 - TEXT_WIDTH) / 2;
     props.y = (240 - TEXT_HEIGHT) / 2;
-    props.dx = 1;
-    props.dy = 2;
+    props.dx = 0;
+    props.dy = 0;
+    props.x_multiplier = 0;
+    props.y_multiplier = 0;
 
     props.inverted = 0;
     props.scaled = 1;
@@ -66,8 +77,8 @@ void menu_item_scale_callback(void *userdata) {
 
     props.x = (400 - TEXT_WIDTH * props.scaled) / 2 / props.scaled;
     props.y = (240 - TEXT_HEIGHT * props.scaled) / 2 / props.scaled;
-    props.dx = 1;
-    props.dy = 2;
+//    props.dx = 1;
+//    props.dy = 2;
 
     pd->display->setScale(props.scaled);
     eventHandler(pd, kEventResume, 0);
@@ -98,34 +109,79 @@ void initialize_menu(PlaydateAPI *pd) {
 
 
 void buttonHandler(PlaydateAPI *pd) {
-    PDButtons current;
     PDButtons pushed;
-    PDButtons released;
-    pd->system->getButtonState(&current, &pushed, &released);
+    pd->system->getButtonState(NULL, &pushed, NULL);
 
     // Reverse the direction if appropriate.
-    if ((released & kButtonRight && props.dx < 0)
-        || (released & kButtonLeft && props.dx > 0))
+    if ((pushed & kButtonRight && props.dx < 0)
+        || (pushed & kButtonLeft && props.dx > 0))
         props.dx = -props.dx;
-    if ((released & kButtonDown && props.dy < 0)
-        || (released & kButtonUp && props.dy > 0))
+    if ((pushed & kButtonDown && props.dy < 0)
+        || (pushed & kButtonUp && props.dy > 0))
         props.dy = -props.dy;
+}
 
-//    pd->system->logToConsole("Button state: current=%d, pushed=%d, released=%d\n",
-//                             current, pushed, released);
+int min(int i1, int i2) {
+    return i1 < i2 ? i1 : i2;
+}
+
+int max(int i1, int i2) {
+    return i1 > i2 ? i1 : i2;
+}
+
+void crankHandler(PlaydateAPI *pd) {
+    // If the crank is docked or there is no change, return.
+    if (pd->system->isCrankDocked())
+        return;
+    float crank_change = pd->system->getCrankChange();
+    if (crank_change == 0)
+        return;
+
+    // Change the speed depending on the angle of the crank.
+    float crank_angle = pd->system->getCrankAngle();
+
+    // If there is no movement, start the movement.
+    // If the crank change is positive, move in positive direction.
+    if (crank_change > 0 && !props.dx && !props.dy) {
+        props.dx = X_DELTA;
+        props.dy = Y_DELTA;
+    }
+    // If crank change is negative, move in negative direction.
+    if (crank_change < 0 && !props.dx && !props.dy) {
+        props.dx = -X_DELTA;
+        props.dy = -Y_DELTA;
+    }
+
+    // If crank change is positive and has moved the crank from before 360 to after 0, accelerate.
+    // Example: crank_angle = 5, crank_change = 10 -> change (was originally 355).
+    // Example: crank_angle = 5, crank_change = 2 -> do not change (was originally 3).
+    // Example: crank_angle = 359, crank_change = 10 -> do not change (was originally 349).
+    if (crank_change > 0 && (crank_angle - crank_change) < 0) {
+        props.x_multiplier = min(X_MULTIPLIER_MAX, props.x_multiplier + X_MULTIPLIER_DELTA);
+        props.y_multiplier = min(Y_MULTIPLIER_MAX, props.y_multiplier + Y_MULTIPLIER_DELTA);
+    }
+    // If crank change is negative and has moved the crank from after 0 to before 360, decelerate.
+    // Example: crank_angle = 355, crank_change = -10 -> change (was originally 5).
+    // Example: crank_angle = 355, crank_change = -2 -> do not change (was originally 357).
+    // Example: crank_angle = 1, crank_change = -10 -> do not change (was originally 11).
+    if (crank_change < 0 && (crank_angle - crank_change) > 360) {
+        props.x_multiplier = max(0, props.x_multiplier - X_MULTIPLIER_DELTA);
+        props.y_multiplier = max(0, props.y_multiplier - Y_MULTIPLIER_DELTA);
+    }
+    pd->system->logToConsole("Crank change is %.2f, angle is %.2f", (double)crank_change, (double)crank_angle);
+    pd->system->logToConsole("x_multiplier=%d, y_multiplier=%d", props.x_multiplier, props.y_multiplier);
 }
 
 
-int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg) {
+int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t) {
 	if (event == kEventInit) {
-        pd->system->logToConsole("kEventInit event\n");
-
 		const char* err;
 		font = pd->graphics->loadFont(font_path, &err);
 		
 		if (font == NULL)
 			pd->system->error("%s:%i Couldn't load font %s: %s", __FILE__, __LINE__, font_path, err);
 
+        // Initialize the properties and the menu items.
         initialize_properties();
         initialize_menu(pd);
 
@@ -134,27 +190,24 @@ int eventHandler(PlaydateAPI* pd, PDSystemEvent event, uint32_t arg) {
 		pd->system->setUpdateCallback(update, pd);
 	}
 
-    if (event == kEventKeyReleased && props.dx < 0) {
-        pd->system->logToConsole("kEventKeyReleased event\n");
-        props.dx = -props.dx;
-    }
-
 	return 0;
 }
 
 
-static int update(void* userdata)
-{
+static int update(void* userdata) {
 	PlaydateAPI* pd = userdata;
+
+    // Check for directional or speed changes based on buttons and crank.
     buttonHandler(pd);
+    crankHandler(pd);
 
 	pd->graphics->clear(kColorWhite);
 	pd->graphics->setFont(font);
 	pd->graphics->drawText("Hello World!", strlen("Hello World!"), kASCIIEncoding, props.x, props.y);
 
     // Change the x and y coordinates, flipping the speed if necessary.
-	props.x += props.dx;
-	props.y += props.dy;
+	props.x += props.dx * props.x_multiplier;
+	props.y += props.dy * props.y_multiplier;
 	if (props.x < 0 || props.x * props.scaled > (LCD_COLUMNS - TEXT_WIDTH * props.scaled))
 		props.dx = -props.dx;
 	if (props.y < 0 || props.y * props.scaled > (LCD_ROWS - TEXT_HEIGHT * props.scaled))
